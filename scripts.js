51131f1a6b84b53c48437267e0ecb8099ee58ffa
Wesslee Whatley <wesslee.whatley@gmail.com>
2020-09-29 19:29:21 -0500

    add core files and basic functions

---
+++ scripts.js
@@ -0,0 +1,42 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    switch(operator) {
+        case "+":
+            console.log(add(num1, num2));
+            break;
+        case "-":
+            console.log(subtract(num1, num2));
+            break;
+        case "*":
+            console.log(multiply(num1, num2));
+            break;
+        case "/":
+            console.log(divide(num1, num2));
+            break;
+        default:
+            console.log("Error");
+    }
+}
+
+let operator = prompt("Enter an operator");
+let num1 = prompt("Enter a number");
+let num2 = prompt("Enter a second number");
+
+operate(operator, num1, num2);
